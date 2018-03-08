49be0b4b6d205814bfeb638395c51ea52ae1fac4
Viktor Bogutskii <info@siteograf.com>
2018-03-07 23:32:35 -0800

    todo list

--- src/layout/App.js
+++ src/layout/App.js
@@ -2,10 +2,42 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todoInput: '',
+      todoList: []
+    };
+  }
+
+  addTodo(){
+
+    const todoList = this.state.todoList;
+    todoList.push(this.state.todoInput);
+
+    this.setState({
+      todoList,
+      todoInput: '',
+    });
+  }
+
   render() {
     return (
       <div>
-        React
+
+        <input
+          type="text"
+          value={this.state.todoInput}
+          onChange={(e) => this.setState({ todoInput: e.target.value })}
+        />
+
+        <button onClick={() => this.addTodo()}>Add</button>
+
+        <ul>
+          {this.state.todoList.map(el => <li key={el}>{el}</li>)}
+        </ul>
+
       </div>
     );
   }
