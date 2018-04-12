a27eb62485299c3680060eecb14771fdff0f2ded
Viktor Bogutskii <info@siteograf.com>
2018-04-11 21:59:35 -0700

    example fetch

--- src/layout/App.js
+++ src/layout/App.js
@@ -5,84 +5,68 @@ class App extends Component {
     super(props);
 
     this.state = {
-      todoInput: '',
-      todoList: [{ id: 1, name: 'Temp' }, { id: 2, name: 'New' }],
+      list: [],
+      a: 0,
+      b: 0,
+      result: 0,
     };
   }
 
-  uuidv4() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+  load() {
+    console.log('Load');
+    this.setState({ list: ['First', 'Second'] });
+
+    fetch('http://localhost:5000/product', { method: 'POST' })
+      .then(response => response.json())
+      .then(json => console.log(json));
   }
 
-  addTodo() {
-    const todoList = this.state.todoList;
-    todoList.push({
-      id: this.uuidv4(),
-      name: this.state.todoInput,
+  sum() {
+    const body = JSON.stringify({
+      a: this.state.a,
+      b: this.state.b,
     });
 
-    this.setState({
-      todoList,
-      todoInput: '',
-      todoInputValid: false,
-    });
-  }
-
-  inputChange(v) {
-    const todoInputValid = v.trim().length >= 3;
-
-    this.setState({
-      todoInput: v,
-      todoInputValid,
-    });
-  }
-
-  deleteItem(id) {
-    const todoList = this.state.todoList.filter(el => el.id !== id);
-
-    this.setState({ todoList });
+    fetch(
+      'http://localhost:5000/calc/sum',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body,
+      },
+    )
+      .then(response => response.json())
+      .then((json) => {
+        this.setState({ result: json.result });
+        console.log(json);
+      });
   }
 
   render() {
     return (
       <div className="container pt-4">
         <div className="row">
+          <input className="form-control" type="text" onChange={e => this.setState({ a: +e.target.value })} />
+          <input className="form-control" type="text" onChange={e => this.setState({ b: +e.target.value })} />
 
-          <div className="col-6  col-sm-4 col-md-6 col-lg-4 border">
-            <ul>
-              {this.state.todoList.map(el =>
-                (
-                  <li key={el.id}>{el.name}
-                    <span
-                      className="btn btn-danger btn-sm"
-                      onClick={() => this.deleteItem(el.id)}
-                    >Delete
-                    </span>
-                  </li>))}
-            </ul>
-          </div>
+          <div className="btn btn-primary" onClick={() => this.sum()}>Sum</div>
 
-          <div className="col-12 col-sm-4 col-md-6 col-lg-4">
-            <div className="input-group">
-              <input
-                className="form-control"
-                type="text"
-                value={this.state.todoInput}
-                onChange={e => this.inputChange(e.target.value)}
-              />
-              <div className="input-group-append">
-                <button
-                  disabled={!this.state.todoInputValid}
-                  className="btn btn-outline-primary"
-                  onClick={() => this.addTodo()}
-                >Add
-                </button>
-              </div>
-            </div>
-          </div>
-
-
+          <div className="btn btn-primary" onClick={() => this.load()}>Load</div>
         </div>
+
+        <div className="row">
+
+          <h2>{this.state.result}</h2>
+
+          <ul>
+            {
+              this.state.list.map(el => <li key={el}>{el}</li>)
+            }
+          </ul>
+        </div>
+
       </div>
     );
   }
